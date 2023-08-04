b6b912e8555dc8a8b33a9365528f2a5a9617ae47
AvazbekDev <126749705+Avazbek-Abduraxmonov@users.noreply.github.com>
2023-08-04 11:14:17 +0500

    function bilan ishlaymiz

---
+++ function_2.py
@@ -0,0 +1,32 @@
+# Sariq dev def function hammasini ishlaymiz 
+
+# python har doim birinchi bo'lgan
+
+# def function(name):
+#     names = []
+#     for i in name:
+#         capitalized_text = i.capitalize()
+#         names.append(capitalized_text)
+#     return names
+
+# texts = ["avazbek","javohir","vasliddin"]
+# capitalized_text = function(texts)
+# print(texts)
+# print(capitalized_text)
+
+# *args **kwargs
+
+# def function(*numbers):
+#     # """Sonlarni kopaytmasini topadigan dastur"""
+#     opacity = 1
+#     for i in numbers:
+#         opacity *= i
+#     return opacity
+# result = function(2,3,4)
+# print(result)
+
+
+
+# modullar  youtube dan sariq dev deb qidirib koring va shunda modullar degan darslari bor shuni koring va o'zingizda takrorlang
+
+# modullar da amaliyot yoq ekan shuni uchun buni o'zingiz qilib koring
